91fc6c0470c399f5de93890fa3f6b32b6a0ac55a
ismayel-shekh <md.shekh.ismayel@gmail.com>
2024-03-21 17:46:19 +0600

    first change

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-96$+%!-p#364%32=ufqrdh_v%62^u8gu%k302ymsyu6givl65v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
